c6baf00b4aef53c68887feaa896d2c86053d8d8a
Henrique Fonseca <henriquefons@hotmail.com>
2021-04-25 13:43:59 -0300

    fix: cores na tela do dashoboard

--- src/components/Slide/Itens/SlideItens.js
+++ src/components/Slide/Itens/SlideItens.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { IconButton } from '@material-ui/core';
+import { Grid, IconButton } from '@material-ui/core';
 import './SlideItens.css';
 
 const SlideItens = ({ ...children }) => (
-  <div className="slide-itens">
+  <Grid className="slide-itens" onClick={children.action}>
     <IconButton>{children.icon}</IconButton>
     <span className="slide-itens__name">{children.name}</span>
-  </div>
+  </Grid>
 );
 
 export default SlideItens;
--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 import './Slide.css';
 import { IconButton, InputBase } from '@material-ui/core';
@@ -6,10 +6,24 @@ import { MenuOpen } from '@material-ui/icons';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import { useHistory } from 'react-router-dom';
 import SlideItens from './Itens/SlideItens';
+import StoreContext from '../Store/context';
 
 const Slide = () => {
   const [open, setOpen] = useState(false);
+  const { setToken } = useContext(StoreContext);
+  const history = useHistory();
+
+  const logout = () => {
+    setToken(null);
+  };
+
+  const changePage = (route) => {
+    if (history.location.pathname !== route) {
+      history.push(route);
+    }
+  };
 
   return (
     <>
@@ -31,12 +45,18 @@ const Slide = () => {
           <SlideItens
             icon={<AccountCircleIcon fontSize="large" />}
             name="Perfil"
+            action={() => changePage('/usuario')}
           />
           <SlideItens
             icon={<PersonAddIcon fontSize="large" />}
             name="Adicionar usuário"
+            action={() => changePage('/usuario/cadastrar')}
           />
-          <SlideItens icon={<ExitToAppIcon fontSize="large" />} name="Sair" />
+          <SlideItens
+            icon={<ExitToAppIcon fontSize="large" />}
+            name="Sair"
+            action={() => logout()}
+          />
         </div>
       </div>
     </>
