ab926c8721e31519dd169ebbc8d90b8f8a20da9a
Henrique Fonseca <henriquefons@hotmail.com>
2021-04-24 21:38:22 -0300

    feat: layout login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,38 +1,77 @@
 import { Button, TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 
-const Login = () => (
-  <div className="login">
-    <div className="login__form">
-      <form className="login__form-container">
-        <h3 className="login__form-title">Login</h3>
-        <div className="login__form-input">
-          <TextField
-            placeholder="Insira o nome do usuário"
-            label="Usuário"
-            fullWidth
-          />
-          <TextField
-            placeholder="Insira a senha do usuário"
-            label="Senha"
-            type="password"
-            fullWidth
-          />
-        </div>
-        <div className="login__form-actions">
-          <Button variant="contained" color="primary">
-            Entrar
-          </Button>
-        </div>
-      </form>
+const initialValues = {
+  user: '',
+  password: '',
+};
+
+const Login = () => {
+  const [values, setValues] = useState(initialValues);
+
+  const changeValues = (event) => {
+    const { name, value } = event.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  function onSubmit(ev) {
+    ev.preventDefault();
+
+    console.log(values);
+  }
+
+  return (
+    <div className="login">
+      <div className="login__form">
+        <form className="login__form-container" onSubmit={onSubmit}>
+          <h3 className="login__form-title">Login</h3>
+          <div className="login__form-input">
+            <TextField
+              value={values.user}
+              onChange={changeValues}
+              name="user"
+              placeholder="Insira o nome do usuário"
+              label="Usuário"
+              fullWidth
+            />
+            <TextField
+              value={values.password}
+              name="password"
+              onChange={changeValues}
+              placeholder="Insira a senha do usuário"
+              label="Senha"
+              type="password"
+              fullWidth
+            />
+          </div>
+          <div className="login__form-actions">
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              onClick={() => setValues(initialValues)}
+            >
+              Cancelar
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="large"
+              type="submit"
+            >
+              Entrar
+            </Button>
+          </div>
+        </form>
+      </div>
+      <div className="login__content">
+        <p className="login__content-logo">
+          Creative <span>Code</span>
+        </p>
+      </div>
     </div>
-    <div className="login__content">
-      <p className="login__content-logo">
-        Creative <span>Code</span>
-      </p>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Login;
---
+++ src/pages/Login/User/LoginUser.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Login from '../../../components/Login/Login';
+
+const LoginUser = () => <Login />;
+
+export default LoginUser;
